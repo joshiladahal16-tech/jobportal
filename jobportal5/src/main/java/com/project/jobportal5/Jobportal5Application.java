6c85224452512ae0f1f944bb7a12a15d7e2ad704
joshila <joshiladahal16@gmail.com>
2025-08-28 15:53:19 +0545

    Job portal Spring Boot application

---
+++ jobportal5/src/main/java/com/project/jobportal5/Jobportal5Application.java
@@ -0,0 +1,13 @@
+package com.project.jobportal5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jobportal5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jobportal5Application.class, args);
+	}
+
+}
